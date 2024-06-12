e2de8303e00425c1298bd56ef4e91324b38d9780
Jeremias.Oviedo <jeremiasoviedo01@mail.com>
2024-06-12 19:04:48 -0300

    se sacó la publicidad de aesa y se puso ovienet

--- src/views/layouts/difusion.php
+++ src/views/layouts/difusion.php
@@ -3,16 +3,16 @@
     <div class="slider-frame">
         <ul>
             <li>
-                <img src="./src/assets/img/difusion/gasistaLeoOviedo.webp" alt="gasista-leo-oviedo" />
+                <img src="./src/assets/img/difusion/ovienet.png" alt="gasista-leo-oviedo" />
             </li>
             <li>
-                <img src="./src/assets/img/difusion/AESAkokiatala.webp" alt="AESA" />
+                <img src="./src/assets/img/difusion/ovienet.png" alt="AESA" />
             </li>
             <li>
                 <img src="./src/assets/img/difusion/Tapicerialagaviota.webp" alt="tapiceria-la-gaviota" />
             </li>
             <li>
-                <img src="./src/assets/img/difusion/AESAkokiatala.webp" alt="AESA" />
+                <img src="./src/assets/img/difusion/gasistaLeoOviedo.webp" alt="AESA" />
             </li>
         </ul>
     </div>
@@ -20,10 +20,10 @@
     <div class="slider-frame">
         <ul>
             <li>
-                <img src="./src/assets/img/difusion/AESAkokiatala.webp" alt="AESA" />
+                <img src="./src/assets/img/difusion/ovienet.png" alt="AESA" />
             </li>
             <li>
-                <img src="./src/assets/img/difusion/aesa2.webp" alt="AESA-2" />
+                <img src="./src/assets/img/difusion/ovienet.png" alt="AESA-2" />
             </li>
             <li>
                 <img src="./src/assets/img/difusion/gasistaLeoOviedo.webp" alt="gasista-leo-oviedo" />
